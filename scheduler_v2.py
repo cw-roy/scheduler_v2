3c59731f9dac023790fb3db361bb5a21a895e9c2
cw-roy <curtis_roy@msn.com>
2024-02-11 11:42:56 -0800

    Add recent updates, modify .gitignore

--- scheduler_v2.py
+++ scheduler_v2.py
@@ -23,10 +23,14 @@ logging.basicConfig(
     ],
 )
 
+# Constants
+NUM_WEEKS = 52
+
 # Set the paths to the input and output files
-team_list_path = os.path.join("data", "team_list.xlsx")
+team_list_path = os.path.join(data_directory, "team_list.xlsx")
+assignment_path = os.path.join(data_directory, "assignments.xlsx")
 assignment_data_log_path = os.path.join(log_directory, "assignment_data_log.csv")
-history_directory = os.path.join(os.path.dirname(__file__), "history")
+
 
 def read_employee_data(file_path):
     try:
@@ -39,15 +43,17 @@ def read_employee_data(file_path):
         return None
 
 def load_and_detect_changes(current_employee_data, previous_employee_data):
+    logging.info("Load and detect changes in employee data")
+    print("Loading and detecting changes in employee data...")
     if previous_employee_data is not None:
         changes = detect_changes(previous_employee_data, current_employee_data)
         if changes:
             logging.info("Changes detected in team_list.xlsx:")
             print("Changes detected in team_list.xlsx:")
-
             for change in changes:
                 logging.info(change)
                 print(change)
+
 
 def detect_changes(old_data, new_data):
     changes = []
@@ -65,19 +71,25 @@ def detect_changes(old_data, new_data):
 
     return changes
 
+
 def log_activity(activity_description):
     formatted_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"{formatted_timestamp} - {activity_description}")
     print(f"{formatted_timestamp} - {activity_description}")
 
+
 def backup_existing_assignments():
+    logging.info("Back up existing assignments")
+    print("Backing up existing assignments...")
     if os.path.exists(assignment_data_log_path):
         timestamp_str = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
-        new_file_name = os.path.join(history_directory, f"assignment_data_log_as_of_{timestamp_str}.csv")
+        new_file_name = os.path.join(
+            history_directory, f"assignment_data_log_as_of_{timestamp_str}.csv"
+        )
 
         os.rename(assignment_data_log_path, new_file_name)
-        logging.info(f"Assignment data log backed up to {new_file_name}")
-        print(f"Assignment data log backed up to {new_file_name}")
+        logging.info(f"Back up {assignment_path} to {new_file_name}")
+        print(f"Assignment log backed up to {new_file_name}")
 
         # Keep only the last 3 backups, delete older ones
         delete_old_backups(history_directory, "assignment_data_log", keep_latest=3)
@@ -85,33 +97,44 @@ def backup_existing_assignments():
         logging.info("No existing assignment data log to back up.")
         print("No existing assignment data log to back up.")
 
+
 def delete_old_backups(directory, base_name, keep_latest):
+    logging.info(f"Delete old backups in {directory}")
+    print(f"Deleting old backups in {directory}...")
     files = [f for f in os.listdir(directory) if f.startswith(f"{base_name}_as_of_")]
     files.sort(key=lambda x: os.path.getctime(os.path.join(directory, x)), reverse=True)
 
     # Delete older backups, keep only the last 'keep_latest'
     for old_file in files[keep_latest:]:
         os.remove(os.path.join(directory, old_file))
-        logging.info(f"Retain maximum of {keep_latest} saved logs. Old {base_name} backup deleted: {old_file}")
-        print(f"Retain maximum of {keep_latest} saved logs. Old {base_name} backup deleted: {old_file}")
+        logging.info(
+            f"Retain maximum of {keep_latest} saved logs. Old {base_name} backup deleted: {old_file}"
+        )
+        print(
+            f"Retain maximum of {keep_latest} saved logs. Old {base_name} backup deleted."
+        )
+
+
 
 def main():
+    logging.info("Start script execution")
+    print("Starting script execution...")
     current_employee_data = read_employee_data(team_list_path)
     previous_employee_data = read_employee_data(team_list_path)
 
     if current_employee_data is not None:
         load_and_detect_changes(current_employee_data, previous_employee_data)
 
-        log_activity("Backing up existing assignments.")
-        print("Backing up existing assignments.")
-
         backup_existing_assignments()
 
-        log_activity("Script execution completed with changes.\n")
-        print("Script execution completed with changes.\n")
+        # generate_schedule_weeks()
+
+        logging.info("Script execution completed.\n")
+        print("Script execution completed.\n")
     else:
         logging.error("Exiting program due to errors.\n")
         print("Exiting program due to errors.\n")
+
 
 if __name__ == "__main__":
     main()
